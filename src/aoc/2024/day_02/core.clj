4beccd7e758533182bff0cbddf4c7e4107f39f8b
Bartev Vartanian <bartev@gmail.com>
2024-12-02 01:17:32 -0600

    Day 2 download data

---
+++ src/aoc/2024/day_02/core.clj
@@ -0,0 +1,10 @@
+(ns aoc.2024.day-02.core
+    (:require
+     [aoc.file-util :as f]
+     [aoc.string-util :as s]))
+
+(def input (f/read-file "2024/d02.txt"))
+
+(defn part-1 [input] true)
+
+(defn part-2 [input] true)
