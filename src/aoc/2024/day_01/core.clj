1b50341cc604ee697b100fce01f11df572e42165
Bartev Vartanian <bartev@gmail.com>
2024-12-02 00:26:23 -0600

    2024-12-01 part 1

---
+++ src/aoc/2024/day_01/core.clj
@@ -0,0 +1,10 @@
+(ns aoc.2024.day-01.core
+    (:require
+     [aoc.file-util :as f]
+     [aoc.string-util :as s]))
+
+(def input (f/read-file "2024/d01.txt"))
+
+(defn part-1 [input] true)
+
+(defn part-2 [input] true)
