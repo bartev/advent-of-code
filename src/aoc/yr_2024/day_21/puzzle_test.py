d24e37a47249a7e8f2b691f8817cd0355d12ba2e
Bartev Vartanian <bartev@gmail.com>
2024-12-28 16:23:47 -0600

    Day 21 part 2 (solved with help)
    Lot's of help.
    
    Ok, I copied the solution from reddit and still don't understand it.

--- src/aoc/yr_2024/day_21/puzzle_test.py
+++ src/aoc/yr_2024/day_21/puzzle_test.py
@@ -2,13 +2,35 @@
 
 import json
 
-import puzzle as p
+import aoc.yr_2024.day_21.puzzle as p
 import pytest
 
 
-def test_part1():
-    assert 1 == 1
+# def test_part1():
+#     assert 1 == 1
 
 
-def test_part2():
-    assert 1 == 1
+# def test_part2():
+#     assert 1 == 1
+
+
+def test_filter_shortest_paths():
+    buttons_1 = [["<"], ["^"], ["^^>", "^>^", ">^^"], ["vvv"]]
+    buttons_2 = [
+        ["<"],
+        ["^", "A", "BB"],
+        ["^^>", "^>^", ">^^", "CCC", "ccc", "CDCD"],
+        ["vvv"],
+    ]
+
+    buttons_2_expected = [
+        ["<"],
+        ["^", "A"],
+        ["^^>", "^>^", ">^^", "CCC", "cccd"],
+        ["vvv"],
+    ]
+    assert buttons_1 == p.filter_shortest_paths(buttons_1)
+    assert buttons_2_expected == p.filter_shortest_paths(buttons_2)
+
+
+test_filter_shortest_paths()
