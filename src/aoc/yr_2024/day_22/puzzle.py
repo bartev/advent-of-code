8dfec1b157127d8c484a43c871799f1e934835f3
Bartev Vartanian <bartev@gmail.com>
2024-12-31 19:29:02 -0600

    Day 22 stubs

---
+++ src/aoc/yr_2024/day_22/puzzle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import logging
+from pathlib import Path
+
+import rich
+from rich.logging import RichHandler
+from rich.panel import Panel
+from rich.rule import Rule
+
+from aoc.pyutils.utils import time_it
+
+# Set up basic config for logging
+FORMAT = "%(levelname)8s - %(funcName)s - %(message)s"
+logging.basicConfig(level="NOTSET", format=FORMAT, handlers=[RichHandler()])
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+dname = Path("../../../../resources/2024/")
+fname = dname / "d22.txt"
+FNAME_TEST = "test_data.txt"
+
+
+def read_data(filename: str):
+    """Read the data into rules and pages"""
+    with open(filename, "r", encoding="utf8") as f:
+        content = f.read()
+    return content
+
+
+# ########## Part 1
+
+rich.print(Rule("Part 1", style="bold green"))
+rich.print(Panel.fit("[bold green]Part 1"))
+
+
+@time_it
+def part1(filename: str) -> int:
+    """Run part 1 given the input file
+    Return value should be the solution"""
+
+
+rich.print(f"""test data: {part1(FNAME_TEST)}""")
+# rich.print(f"""Problem input: {part1(fname)}""")
+
+# ########## Part 2
+
+rich.print(Rule("Part 2", style="bold red"))
+rich.print(Panel.fit("[bold red]Part 2"))
+
+
+@time_it
+def part2(filename: str) -> int:
+    """Run part 2 given the input file
+    Return value should be the solution"""
+
+
+# rich.print(f"""test data: {part2(FNAME_TEST)}""")
+rich.print(f"""Problem input: {part2(fname)}""")
